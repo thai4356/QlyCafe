c3c2f8e831e3980ca34a584f9532af2549b4e391
thai4356 <tranquocthai5296@gmail.com>
2025-05-23 00:37:33 +0700

    23/5

--- QlyCafe/Class/Function.cs
+++ QlyCafe/Class/Function.cs
@@ -11,7 +11,7 @@ namespace QlyCafe
 {
     internal class Function
     {
-        private static string connString = "Data Source=DESKTOP-6P76JI8\\SQLEXPRESS;Initial Catalog=qlyCafe;Integrated Security=True;TrustServerCertificate=True";
+        private static string connString = "Data Source=LAPTOP-FRNPC1AU;Initial Catalog=qlyCafe;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
         private static SqlConnection conn;
 
         public static void OpenConnection()
